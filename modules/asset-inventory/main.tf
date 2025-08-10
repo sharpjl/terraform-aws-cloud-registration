f2a408caabf0723d6ce1b127cf79d2dbfdf9916d
Ashoksankar Hariharasubramony <ashoksankar.hariharasubramony@crowdstrike.com>
2025-08-09 22:28:14 -0700

    AWS registration: Add missing documention for new variable to control IAM reader role creation

--- modules/asset-inventory/main.tf
+++ modules/asset-inventory/main.tf
@@ -33,9 +33,9 @@ resource "aws_iam_role" "this" {
 }
 
 resource "aws_iam_role_policy" "this" {
-  name   = "cspm_config"
-  count  = !var.use_existing_iam_reader_role ? 1 : 0
-  role   = aws_iam_role.this[count.index].id
+  name  = "cspm_config"
+  count = !var.use_existing_iam_reader_role ? 1 : 0
+  role  = aws_iam_role.this[count.index].id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
